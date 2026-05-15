aa1e7744f5db2e7a7010636d2992724c6974d1c5
Sayyad Sofiyan <sofiyan9545@gmail.com>
2026-05-15 21:58:11 +0530

    Implement core application features

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
 
-const connectDB = async() =>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI, {})
-        console.log("MongoDB connected successfully");
-    }catch(error){
-        console.error("MongoDB connection failed:", error.message);
-        process.exit(1); // Exit process with failure
-    }
-}
+import dns from "dns";
 
-export {connectDB}
+dns.setDefaultResultOrder("ipv4first");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {});
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+export { connectDB };
--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = "https://mern-e-book-backend.onrender.com/api";
-export const BASE_URL = "https://mern-e-book-backend.onrender.com";
+const API_BASE_URL = "https://mern-e-book-backend.onrender.com/api" || "http://localhost:8000/";
+export const BASE_URL = "https://mern-e-book-backend.onrender.com" || "http://localhost:8000/";
 
 export const API_PATHS = {
   AUTH: {
